ebfe9af113fe59bee0a3611b24cff298df2f9529
ardevi44 <arbey9621@gmail.com>
2025-04-20 01:33:11 -0600

    Heads and Tails simulator

--- task.py
+++ task.py
@@ -1,6 +1,12 @@
 # Using some modules ...
 import random
-import my_module
+# import my_module
 
-print(random.randint(1, 10))
-print(my_module.favorite_number)
+# print(random.randint(1, 10))
+# print(my_module.favorite_number)
+# print(random.random() * 10)
+
+if random.randint(0, 1) == 0:
+    print("Heads")
+else:
+    print("Tails")
