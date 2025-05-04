9a6942b46801ced92be6019c5444832e358cf1f1
ardevi44 <arbey9621@gmail.com>
2025-05-03 22:48:25 -0600

    Delete comments in hangman.py

--- hangman.py
+++ hangman.py
@@ -18,14 +18,11 @@ for position in range(word_length):
 
 print(placeholder)
 
-# TODO-5 - Use a while loop to let the user guess again.
 game_over = False
 correct_letters = []
 
 while not game_over:
-    # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
     guess = input("Guess a letter: ").lower()
-    # TODO-3 - Create a "display" that puts the guess letter in the right position and a _ in the rest of the string
     # Change the for loop so that you keep the previous correct letters in display
     display = ""
     for letter in chosen_word:
