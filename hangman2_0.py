c57ec9335a52af40a238695d43ea51f27f31c85f
ardevi44 <arbey9621@gmail.com>
2025-05-03 22:50:47 -0600

    Improve the display messages of the game
    Shows the right leg of the stickman at the end
    of the game.

--- hangman2_0.py
+++ hangman2_0.py
@@ -1,10 +1,8 @@
-# import random
 import os
 import stickman
 
 current_try = 1
 number_guesses = len(stickman.draw) - 1
-# print(number_guesses)
 final_guess_complete = False
 final_guess = ""
 alt_secret_word = ""
@@ -34,76 +32,71 @@ def show_secret_word_wo_underscores(list_final_guess):
 
 def replace_user_coincidences(guess_letter, list_secret_word, list_final_guess):
     index = 0
-    # This loop will put into the result word all the coincidences of the guess
     for index in range(0, len(list_secret_word)):
         if (guess_letter.lower() == list_secret_word[index]) or (guess_letter.upper() == list_secret_word[index]):
             list_final_guess[index] = list_secret_word[index]
             list_secret_word[index] = " "
-    # End this for loop
-
-    # take the result word if it was updated and the secret word left
     final_guess = "".join(list_final_guess)
     alt_secret_word = "".join(list_secret_word)
 
 
 def draw_hangman(current_try):
-    print(stickman.draw[current_try - 1])
+    print(stickman.draw[current_try])
 
 
-def show_tries_left(guesses, current_try):
-    draw_hangman(current_try)
-    if (guesses - current_try) == 0:
-        print(f"Last chance")
+def show_tries_left(number_guesses, current_try):
+    if (number_guesses - current_try) == 0:
+        print(f"This is your last chance!")
     else:
-        print(f"Tries left: {guesses - (current_try - 1)}")
+        print(f"Tries left: {number_guesses - (current_try - 1)}")
 
 
-# Ask the secret word
 secret_word = input(
     "Type the secret word (Be careful with spaces): ")
-
-# pass the secret word to a list.
+os.system("cls")
 list_secret_word = list(secret_word)
-# Create an empty list with the same length of the secret word list
 list_final_guess = [" "] * len(secret_word)
-# alternative secret word, prevent mutated the original
 alt_secret_word = secret_word
 
-
 while (current_try <= number_guesses) and (not final_guess_complete):
-    os.system('cls')
+    draw_hangman(current_try - 1)
     show_tries_left(number_guesses, current_try)
     print(f"Secret word:")
     show_secret_word_w_underscores(list_final_guess)
     guess_letter = input("Try to guess one letter or try a complete word: ")
-    if guess_letter == secret_word:  # If the guess is the secret word
+    if guess_letter == secret_word:
         show_secret_word_wo_underscores(guess_letter)
         print(
             f"Excellent the secret word is {guess_letter}, You won the challenge!")
         break
-    elif len(guess_letter) > 1:  # If the length of the guess is greater than just one character
+    elif len(guess_letter) > 1:
+        os.system("cls")
         print(
             "You should enter just one character or the secret word, if you already guessed it.")
-     # If the letter is in the secret word
     elif (guess_letter.lower() in alt_secret_word) or (guess_letter.upper() in alt_secret_word):
         replace_user_coincidences(
             guess_letter, list_secret_word, list_final_guess)
-        if secret_word == final_guess:  # If the user finally found the word
+        os.system("cls")
+        print(f"The letter {guess_letter} is in the word. Well done!")
+        if secret_word == final_guess:
             show_secret_word_wo_underscores(list_final_guess)
-            # Break the loop
             final_guess_complete = True
             current_try = 6
             print(
                 f"Excellent the correct word is {final_guess}, You won the challenge!")
             print("You won the game!")
             break
-    elif current_try == number_guesses:  # The user ends his tries
+    elif current_try == number_guesses:
+        os.system("cls")
         print("Sorry")
         print("You've lose the game")
+        draw_hangman(current_try)
+        print(f"The secret word was: {secret_word}")
         break
-    else:  # The user still have chances but he fail the current try
+    else:
         tries_left = number_guesses - current_try
         current_try += 1
+        os.system("cls")
         if tries_left == 0:
             print("You're in your last chance, Good luck, don't give up!")
         else:
@@ -111,11 +104,6 @@ while (current_try <= number_guesses) and (not final_guess_complete):
 
 
 """
-ISSUES:
-
-#1
-In the last chance of the user print the complete hangman, there is one missing leg (the right one)
-
 #2
 When the word is a compound word, the program has to ignore them and just care about
 of the real characters that compound the world E.g. "Hello World"
