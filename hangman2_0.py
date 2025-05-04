0ee03f035d9271fe34c8fcf6a4e418393a86dc9b
ardevi44 <arbey9621@gmail.com>
2025-05-04 05:52:44 -0600

    Solve 2 issues
    - You can type compound words now
    - Deal when the user types empty or " " or many spaces, just ask
      to type a letter again

--- hangman2_0.py
+++ hangman2_0.py
@@ -10,15 +10,18 @@ alt_secret_word = ""
 # Print the current final guessed word with underscores
 
 
-def show_secret_word_w_underscores(list_final_guess):
-    for blank in list_final_guess:
-        if blank == " ":
-            print(f"_ ", end="")
+def show_secret_word_w_underscores(list_final_guess, alt_secret_word):
+    i = 0
+    for i in range(len(list_final_guess)):
+        if (alt_secret_word[i] == " ") and (list_final_guess[i] == " "):
+            print("  ", end="")
+        elif list_final_guess[i] == " ":
+            print("_ ", end="")
         else:
-            print(f"{blank} ", end="")
+            print(f"{list_final_guess[i]} ", end="")
     print()
 
-# Print the current final guessed word without underscores
+    # Print the current final guessed word without underscores
 
 
 def show_secret_word_wo_underscores(list_final_guess):
@@ -33,11 +36,9 @@ def show_secret_word_wo_underscores(list_final_guess):
 def replace_user_coincidences(guess_letter, list_secret_word, list_final_guess):
     index = 0
     for index in range(0, len(list_secret_word)):
-        if (guess_letter.lower() == list_secret_word[index]) or (guess_letter.upper() == list_secret_word[index]):
+        if (guess_letter == list_secret_word[index]):
             list_final_guess[index] = list_secret_word[index]
             list_secret_word[index] = " "
-    final_guess = "".join(list_final_guess)
-    alt_secret_word = "".join(list_secret_word)
 
 
 def draw_hangman(current_try):
@@ -51,39 +52,67 @@ def show_tries_left(number_guesses, current_try):
         print(f"Tries left: {number_guesses - (current_try - 1)}")
 
 
+def show_secret_word_w_spaces(secret_word):
+    print("\n")
+    for letter in secret_word:
+        print(f"{letter} ", end="")
+    print("\n")
+
+
+def hide_secret_word(list_secret_word):
+    for letter in list_secret_word:
+        if letter != " ":
+            print(f"_ ", end="")
+        else:
+            print(f"  ", end="")
+    print()
+
+
 secret_word = input(
-    "Type the secret word (Be careful with spaces): ")
-os.system("cls")
-list_secret_word = list(secret_word)
+    "Type the secret word: ")
+alt_secret_word = secret_word.lower().strip()
+list_secret_word = list(alt_secret_word)
 list_final_guess = [" "] * len(secret_word)
-alt_secret_word = secret_word
+os.system("cls")
+first_time = True
 
 while (current_try <= number_guesses) and (not final_guess_complete):
     draw_hangman(current_try - 1)
     show_tries_left(number_guesses, current_try)
     print(f"Secret word:")
-    show_secret_word_w_underscores(list_final_guess)
-    guess_letter = input("Try to guess one letter or try a complete word: ")
-    if guess_letter == secret_word:
-        show_secret_word_wo_underscores(guess_letter)
+    if first_time:
+        hide_secret_word(list_secret_word)
+    else:
+        show_secret_word_w_underscores(list_final_guess, alt_secret_word)
+    first_time = False
+    guess_letter = input(
+        "Try to guess one letter or try a complete word: ").lower().strip()
+    if guess_letter == "":
+        os.system("cls")
+        print("At least one character")
+        continue
+    if guess_letter == alt_secret_word:
+        show_secret_word_w_spaces(secret_word)
         print(
-            f"Excellent the secret word is {guess_letter}, You won the challenge!")
+            f"Excellent the secret word is -> {secret_word} <-, You won the challenge!")
         break
     elif len(guess_letter) > 1:
         os.system("cls")
         print(
-            "You should enter just one character or the secret word, if you already guessed it.")
-    elif (guess_letter.lower() in alt_secret_word) or (guess_letter.upper() in alt_secret_word):
+            "Enter just one character or the secret word, if you already guessed it. Try again")
+    elif (guess_letter in alt_secret_word):
         replace_user_coincidences(
             guess_letter, list_secret_word, list_final_guess)
+        final_guess = "".join(list_final_guess)
         os.system("cls")
         print(f"The letter {guess_letter} is in the word. Well done!")
-        if secret_word == final_guess:
+        if alt_secret_word == final_guess:
+            draw_hangman(current_try - 1)
             show_secret_word_wo_underscores(list_final_guess)
             final_guess_complete = True
             current_try = 6
             print(
-                f"Excellent the correct word is {final_guess}, You won the challenge!")
+                f"Excellent the correct word is {secret_word}, You won the challenge!")
             print("You won the game!")
             break
     elif current_try == number_guesses:
@@ -101,13 +130,3 @@ while (current_try <= number_guesses) and (not final_guess_complete):
             print("You're in your last chance, Good luck, don't give up!")
         else:
             print("You've lost one try. Be careful")
-
-
-"""
-#2
-When the word is a compound word, the program has to ignore them and just care about
-of the real characters that compound the world E.g. "Hello World"
-I don't have to introduce a " " character to complete the word. The word just has to include it
-also these spaces have to appear in the secret word instead underscores.
-This will give the user the hint that is a compound word. And probabilities to win increment.
-"""
