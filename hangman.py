f1d00f824906ae7b83b14e27301313195f45fcc5
ardevi44 <arbey9621@gmail.com>
2025-05-24 00:49:43 -0600

    Delete some comments for a better read

--- hangman.py
+++ hangman.py
@@ -4,12 +4,10 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
 chosen_word = random.choice(word_list)
 print(chosen_word)
 print(f"word length {len(chosen_word)}")
 
-# TODO-4 - Create a placeholder with the same number of blanks as the chosen_word
 # NOTE: remember the range function with just one parameter will make a loop of "that parameter" times beginning at 0
 placeholder = ""
 word_length = len(chosen_word)
@@ -23,7 +21,6 @@ correct_letters = []
 
 while not game_over:
     guess = input("Guess a letter: ").lower()
-    # Change the for loop so that you keep the previous correct letters in display
     display = ""
     for letter in chosen_word:
         if letter in correct_letters:
@@ -37,5 +34,3 @@ while not game_over:
     if "_" not in display:
         game_over = True
         print("You win!")
-
-# At this point angela is using two string variables to solve the problem, very lightweight solution.
