9e192ad0c3c44889595f3349ba0ed90d5809a74e
ardevi44 <arbey9621@gmail.com>
2025-05-25 01:49:25 -0600

    Add the hangman stages (to angela's solution)

--- hangman.py
+++ hangman.py
@@ -2,24 +2,92 @@
 # Angela's solution actually
 import random
 
+stages = [
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''',
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''',
+    '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''',
+    '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''',
+    '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''',
+    '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''',
+    '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+'''
+]
+
 word_list = ["aardvark", "baboon", "camel"]
 
+# choose a random word from the list and print it with its length
 chosen_word = random.choice(word_list)
 print(chosen_word)
-print(f"word length {len(chosen_word)}")
+print(f"length {len(chosen_word)}")
 
 # NOTE: remember the range function with just one parameter will make a loop of "that parameter" times beginning at 0
+
+# Takes the placeholder and fill it with underscores as much the chosen word has letters ...
 placeholder = ""
 word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 
-print(placeholder)
-
 game_over = False
 correct_letters = []
 
 while not game_over:
+    # ask the user for a letter ...
     guess = input("Guess a letter: ").lower()
     display = ""
     for letter in chosen_word:
