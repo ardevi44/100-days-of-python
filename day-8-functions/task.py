d77312dbac2c70a08c47df17ff2ae2fdeb1f41d6
ardevi44 <arbey9621@gmail.com>
2025-05-27 12:04:23 -0600

    Positional arguments and keyword arguments

--- day-8-functions/task.py
+++ day-8-functions/task.py
@@ -4,7 +4,7 @@ def greet():
     print("What's the weather like today?")
 
 
-greet()
+# greet()
 
 # Functions that allow for inputs
 
@@ -17,4 +17,20 @@ def greet_with_name(name):
         print("I like your name")
 
 
-greet_with_name("Entrepreneur")
+# greet_with_name("Entrepreneur")
+
+# Function with more than 1 input
+
+
+def greet_with(name, location):
+    print(f"Hi {name.split()[0]}, what is it like in {location.split(",")[0]}")
+
+
+# This is a positional argument behavior, it means if i change the order of the arguments
+# the greeting changes its meaning.
+# greet_with("The middle of nowhere, Kansas city", "Courage the cowardly dog") <- The order has changed
+
+# A way to avoid the positional argument behavior are the keyword arguments
+# Like this ...
+greet_with(location="The middle of nowhere, Kansas city",
+           name="Courage the cowardly dog")
