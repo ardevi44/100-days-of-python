835a3ae6d0587232d4e0156019884efa440c0446
ardevi44 <arbey9621@gmail.com>
2025-06-02 05:06:36 -0600

    Caesar cipher init

---
+++ caesar_cipher_project/caesar_cipher.py
@@ -0,0 +1,70 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# The resulting encrypted or decrypted message
+result = ""
+
+
+def encrypt(original_text, shift_amount):
+    alphabet_last_index = len(alphabet) - 1
+    encrypt_result = ""
+    for letter in original_text:
+        # Just for validation
+        if letter == " ":
+            encrypt_result += letter
+            continue
+        elif not letter in alphabet:
+            encrypt_result += "%"
+            continue
+        # Then ...
+        shifted_index = alphabet.index(letter) + shift_amount
+        if shifted_index > alphabet_last_index:  # We're out of bounds
+            # The difference between last and the current index
+            indexes_diff = alphabet_last_index - alphabet.index(letter)
+            encrypt_result += alphabet[shift_amount - indexes_diff - 1]
+        else:
+            encrypt_result += alphabet[alphabet.index(letter) + shift_amount]
+
+    if len(encrypt_result) != 0:
+        return encrypt_result
+    else:
+        return 0
+
+
+def decrypt(original_text, shift_amount):
+    decrypt_result = ""
+
+    for letter in original_text:
+        if letter == " ":
+            decrypt_result += letter
+            continue
+        elif not letter in alphabet:
+            decrypt_result += "%"
+            continue
+        shifted_index = alphabet.index(letter) - shift_amount
+        decrypt_result += alphabet[shifted_index]
+
+    if len(decrypt_result) != 0:
+        return decrypt_result
+    else:
+        return 0
+
+
+direction = ""
+while direction != "\x04":
+    direction = input(
+        "Type 'encode' to encrypt, type 'decode' to decrypt or Ctrl + D to exit the program:\n").lower()
+    if direction == "encode":
+        original_text = input("Type your message:\n").lower()
+        shift_amount = int(input("Type the shift number:\n"))
+        result = encrypt(original_text=original_text,
+                         shift_amount=shift_amount)
+        print(result)
+    elif direction == "decode":
+        original_text = input("Type your message:\n").lower()
+        shift_amount = int(input("Type the shift number:\n"))
+        result = decrypt(original_text=original_text,
+                         shift_amount=shift_amount)
+        print(result)
+    else:
+        print("Bye bye...")
