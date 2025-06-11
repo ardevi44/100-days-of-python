c648326970b59b9f6f92059f3ada47dabe85e5f1
ardevi44 <arbey9621@gmail.com>
2025-06-10 23:40:27 -0600

    Allow empty spaces and characters

--- caesar_cipher_project/caesar_cipher_angelas_solution.py
+++ caesar_cipher_project/caesar_cipher_angelas_solution.py
@@ -11,9 +11,12 @@ def caesar(original_text, shift_amount, encode_or_decode):
     if encode_or_decode == "decode":
         shift_amount *= -1
     for letter in original_text:
-        shifted_position = alphabet.index(letter) + shift_amount
-        shifted_position = shifted_position % len(alphabet)
-        output_text += alphabet[shifted_position]
+        if not letter in alphabet:
+            output_text += letter
+        else:
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position = shifted_position % len(alphabet)
+            output_text += alphabet[shifted_position]
     custom_message = "decoded" if encode_or_decode == "decode" else "encoded"
     print(
         f"Here is the {custom_message} result: {output_text}")
