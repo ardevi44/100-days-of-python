f3be9c5817a3bf728765853bc3be40be0fd2b327
ardevi44 <arbey9621@gmail.com>
2025-06-10 23:31:04 -0600

    Add the caesar cipher logo

--- caesar_cipher_project/caesar_cipher_angelas_solution.py
+++ caesar_cipher_project/caesar_cipher_angelas_solution.py
@@ -1,5 +1,9 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+print(art.logo)
 
 
 def caesar(original_text, shift_amount, encode_or_decode):
