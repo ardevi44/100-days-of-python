07768a6c66d53e5ab1d1e389839f6fe33488fe55
ardevi44 <arbey9621@gmail.com>
2025-06-10 23:01:39 -0600

    Caesar cipher init angela's solution
    This is the solution given by the course tutor.

---
+++ caesar_cipher_project/caesar_cipher_angelas_solution.py
@@ -0,0 +1,23 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_amount *= -1
+    for letter in original_text:
+        shifted_position = alphabet.index(letter) + shift_amount
+        shifted_position = shifted_position % len(alphabet)
+        output_text += alphabet[shifted_position]
+    custom_message = "decoded" if encode_or_decode == "decode" else "encoded"
+    print(
+        f"Here is the {custom_message} result: {output_text}")
+
+
+direction = input(
+    "Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
