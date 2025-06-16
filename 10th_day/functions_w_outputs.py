ae802e7a5aee0976b6289a8d87d215f23adf86e2
ardevi44 <arbey9621@gmail.com>
2025-06-15 21:36:04 -0600

    Document a function

--- 10th_day/functions_w_outputs.py
+++ 10th_day/functions_w_outputs.py
@@ -3,6 +3,8 @@ def format_name(f_name, l_name):
 
 
 def format_name_2(f_name, l_name):
+    """Take a first and last name and format it to return the title
+    case version of the name."""
     if f_name == "" and l_name == "":
         return "You have tu pass both names"
     elif f_name == "" or l_name == "":
