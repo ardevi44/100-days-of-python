3d9ef5a1865c587621f4d693742c5956065f6950
ardevi44 <arbey9621@gmail.com>
2025-06-26 02:34:48 -0600

    Initial functionality of the game
    - Create a deck of cards (pending add all the cards, just clubs for now)
    - Show the initial amount of money for both players
    - Ask for bets for the player
    - Shuffle the deck
    - Deal the deck
    - Show the score of each hand played for both players

---
+++ 11th_day/cards_deck.py
@@ -0,0 +1,83 @@
+import ascii_cards
+
+cards_deck = [
+    {
+        "name_card": "Two of Clubs",
+        "code_card": "2C",
+        "ascii_art": ascii_cards.clubs_2,
+        "value": 2,
+    },
+    {
+        "name_card": "Three of Clubs",
+        "code_card": "3C",
+        "ascii_art": ascii_cards.clubs_3,
+        "value": 3,
+    },
+    {
+        "name_card": "Four of Clubs",
+        "code_card": "4C",
+        "ascii_art": ascii_cards.clubs_4,
+        "value": 4,
+    },
+    {
+        "name_card": "Five of Clubs",
+        "code_card": "5C",
+        "ascii_art": ascii_cards.clubs_5,
+        "value": 5,
+    },
+    {
+        "name_card": "Six of Clubs",
+        "code_card": "6C",
+        "ascii_art": ascii_cards.clubs_6,
+        "value": 6,
+    },
+    {
+        "name_card": "Seven of Clubs",
+        "code_card": "7C",
+        "ascii_art": ascii_cards.clubs_7,
+        "value": 7,
+    },
+    {
+        "name_card": "Eight of Clubs",
+        "code_card": "8C",
+        "ascii_art": ascii_cards.clubs_8,
+        "value": 8,
+    },
+    {
+        "name_card": "Nine of Clubs",
+        "code_card": "9C",
+        "ascii_art": ascii_cards.clubs_9,
+        "value": 9,
+    },
+    {
+        "name_card": "Ten of Clubs",
+        "code_card": "10C",
+        "ascii_art": ascii_cards.clubs_10,
+        "value": 10,
+    },
+    {
+        "name_card": "Jack of Clubs",
+        "code_card": "JC",
+        "ascii_art": ascii_cards.jack_of_clubs,
+        "value": 10,
+    },
+    {
+        "name_card": "Queen of Clubs",
+        "code_card": "QC",
+        "ascii_art": ascii_cards.queen_of_clubs,
+        "value": 10,
+    },
+    {
+        "name_card": "King of Clubs",
+        "code_card": "KC",
+        "ascii_art": ascii_cards.king_of_clubs,
+        "value": 10,
+    },
+    {
+        "name_card": "Ace of Clubs",
+        "code_card": "AC",
+        "ascii_art": ascii_cards.ace_of_clubs,
+        "is_ace": True
+    },
+
+]
