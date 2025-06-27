ea7ce2a0a182c1dec98c49aac5ff67401ebd3078
ardevi44 <arbey9621@gmail.com>
2025-06-27 01:35:11 -0600

    Separate the logic in different files

---
+++ 11th_day/global_variables.py
@@ -0,0 +1,12 @@
+import numpy as np
+from cards_deck import cards_deck as deck
+copy_deck = np.array(deck).copy()
+player_money = 2500
+player_bet = 0
+player_cards = []
+player_score_cards = 0
+software_money = 2500
+software_bet = 0
+software_cards = []
+software_score_cards = 0
+bets_total = 0
