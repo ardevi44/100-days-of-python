09d920ee0c18c51eccc7db466b6be897a3aa70e9
ardevi44 <arbey9621@gmail.com>
2025-12-10 17:14:32 -0600

    Change the day 9 project
    Improving the code and making the project works

--- 9th_day/secret_auction_program.py
+++ 9th_day/secret_auction_program.py
@@ -1,39 +1,42 @@
 import os
 
 
-def check_for_more_bidders(bidders, user_bids):
-    if bidders == "yes":
+def check_for_more_bidders(more_bidders, bids):
+    """Take an string 'yes'|'no' and a dictionary of bids, and clean the console if there are more bidders
+    and decides the winner if are not"""
+    if more_bidders == "yes":
         os.system("cls")
         return True
     else:
         os.system("cls")
-        say_winner(user_bids)
+        say_winner(bids)
         return False
 
 
-def say_winner(user_bids):
-    max = 0
+def say_winner(bids):
+    """Take the bids and decides which user has won, based on its bids"""
+    greater_bid = 0
+    name = ""
     winner = {}
-    for user in user_bids:
-        if user_bids[user] > max:
-            max = user_bids[user]
-            winner[user] = user
-            winner[bid] = user_bids[user]
-
-    winner_bid_f = "{:.2f}".format(winner[bid])
+    for user, bid in bids.items():
+        if bid > greater_bid:
+            greater_bid = bid
+            name = user
+            winner = {user: greater_bid}
+    winner_bid = "{:.2f}".format(winner[name])
     print(
-        f"The winner is {winner[user].capitalize()} with a bit of ${winner_bid_f}")
+        f"The winner is {name} with a bit of ${winner_bid}")
 
 
 more_bidders = True
-user_bids = {}
+bids = {}
 
 print("Welcome to the secret auction program.")
 
 while (more_bidders):
     user_name = input("What's your name?: ").lower().strip()
     bid = float(input("What's your bid?: $"))
-    user_bids[user_name] = bid
-    bidders = input(
+    bids[user_name] = bid
+    are_more_bidders = input(
         "Are there any other bidders? Type \"yes\" or \"no\": ").lower().strip()
-    more_bidders = check_for_more_bidders(bidders, user_bids)
+    more_bidders = check_for_more_bidders(are_more_bidders, bids)
