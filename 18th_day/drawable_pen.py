9a4b4fbbc730fb0f0e7478a1ac6a027daff1b3af
ardevi44 <arbey9621@gmail.com>
2026-02-15 19:32:03 -0600

    Making some draws via Drawable class

--- 18th_day/drawable_pen.py
+++ 18th_day/drawable_pen.py
@@ -1,5 +1,4 @@
-from turtle import Turtle
-from turtle import Screen
+from turtle import Turtle, Screen
 import random
 
 
@@ -7,12 +6,21 @@ class Drawable:
 
     def __init__(self, distance: int = 10):
         """Initialize a Drawable object with its configuration"""
-        self.s = Screen()
-        self.s.colormode(255)
-        self.t = Turtle()
-        self.t.width(5)
+        self.set_initial_config()
         self.distance = distance
         self.movements = 100
+
+    def set_initial_config(self):
+        # Screen parameters
+        self.s = Screen()
+        self.s.colormode(255)
+        # Pen parameters
+        self.t = Turtle()
+        self.t.speed("normal")
+        self.t.width(5)
+
+    def set_pen_color(self, random_color):
+        self.t.color(random_color)
 
     def move_forward(self, distance: int):
         self.t.forward(distance)
@@ -31,12 +39,13 @@ class Drawable:
     def generate_random_color(self):
         return random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
 
-    def generate_random_movements(self, distance: int = 30):
+    def wait_to_close_window(self):
+        self.s.exitonclick()
+
+    def make_random_movements(self, distance: int = 30):
         while self.movements > 0:
-            tuple_color = self.generate_random_color()
-            self.t.color(tuple_color)
-            number_movement = random.randint(1, 4)
-            match number_movement:
+            self.set_pen_color(self.generate_random_color())
+            match random.randint(1, 4):
                 case 1:
                     self.move_forward(distance)
                 case 2:
@@ -46,4 +55,49 @@ class Drawable:
                 case 4:
                     self.move_backward(distance)
             self.movements -= 1
-        self.s.exitonclick()
+        self.wait_to_close_window()
+
+    def draw_a_square(self):
+        side_length = 100
+        self.t.color(self.generate_random_color())
+        for _ in range(4):
+            self.t.rt(90)
+            self.t.fd(side_length)
+        self.wait_to_close_window()
+
+    def draw_first_six_polygons(self):
+        side_length = 100
+        polygons = 20
+        sides = 3
+        for polygon in range(polygons):
+            self.t.color(self.generate_random_color())
+            for side in range(sides):
+                self.t.forward(side_length)
+                self.t.rt(360 / sides)
+            sides += 1
+        self.wait_to_close_window()
+
+    def draw_spirograph(self, circles):
+        gap_size = 360 / circles
+        circle_count = 0
+        if circles >= 10:
+            self.t.speed("fastest")
+        for circle in range(circles):
+            self.set_pen_color(self.generate_random_color())
+            self.t.circle(100)
+            self.t.setheading(self.t.heading() + gap_size)
+            circle_count += 1
+        print(f"Num circles: {circle_count}, Size of gap: {gap_size}")
+        # Remember wait to close
+
+    def draw_a_dashed_line(self):
+        length = 10
+        num_lines = 10
+        # Initial state down
+        for _ in range(num_lines * 2):
+            self.t.forward(length)
+            if self.t.isdown():
+                self.t.penup()
+            else:
+                self.t.pendown()
+        self.wait_to_close_window()
--- 18th_day/main.py
+++ 18th_day/main.py
@@ -1,91 +1,16 @@
-# from turtle import Turtle, Screen
-# from turtle import Turtle as t
-# from turtle import Screen as s
-# from turtle import *
 from drawable_pen import Drawable
-from turtle import Turtle, Screen
-import random
+
+d = Drawable()
 
 
-def draw_a_square(t_object, side_width):
-    t_object.shape("turtle")
-    t_object.color("red")
-    for side in range(4):
-        t_object.rt(90)
-        t_object.fd(side_width)
+def draw():
+    d.draw_spirograph(25)
 
 
-def draw_a_dashed_line(t, length, num_lines):
-    pen_is_down = True
-    for i in range(num_lines * 2):
-        if pen_is_down:
-            t.forward(length)
-            pen_is_down = False
-        else:
-            t.penup()
-            t.forward(length)
-            t.pendown()
-            pen_is_down = True
+# d.draw_first_six_polygons()
+d.s.listen()
+d.s.onkey(key="s", fun=draw)
+d.wait_to_close_window()
 
-
-def print_pentagon(turtle, side_len):
-    """
-    Draws a simple pentagon with side_len specification.
-
-    Args:
-      turtle (Turtle): A Turtle Object
-      side_len (int): The desired length of the sides
-    Returns:
-      ---
-    """
-    angle = 360 / 5
-    for i in range(5):
-        turtle.forward(side_len)
-        turtle.right(angle)
-
-
-def print_first_six_polygons(t, side_len):
-    polygons_number = 6
-    sides_number = 3
-    for i in range(polygons_number):
-        for i in range(sides_number):
-            degrees = 360 / sides_number
-            t.forward(side_len)
-            t.rt(degrees)
-        sides_number += 1
-
-
-# d = Drawable()
-# d.generate_random_movements()
-s = Screen()
-s.colormode(255)
-t = Turtle()
-t.width(3)
-t.speed("fastest")
-
-
-def draw_spirograph(size_of_gap):
-    for _ in range(int(360 / size_of_gap)):
-        t.color(random.randint(0, 255), random.randint(
-            0, 255), random.randint(0, 255))
-        t.circle(100)
-        t.setheading(t.heading() + size_of_gap)
-
-
-draw_spirograph(15)
-
-s.exitonclick()
-
-"""
-It is another solution
-
-space_available = 360
-angle_required = 5
-number_of_circles = (space_available / angle_required)
-while number_of_circles > 0:
-    t.color(random.randint(0, 255), random.randint(
-        0, 255), random.randint(0, 255))
-    t.circle(100)
-    t.left(angle_required)
-    number_of_circles -= 1
-"""
+# Create a drawable that listen when I press "s" key and start the draw I indicate to the constructor
+# at the creation moment
