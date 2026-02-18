813bcf4542c0e63a7ff446ac37ecc5e9182b742b
ardevi44 <arbey9621@gmail.com>
2026-02-18 12:34:16 -0600

    Add message boxes and validation

--- 19th_day/turtle_race.py
+++ 19th_day/turtle_race.py
@@ -1,16 +1,18 @@
 from turtle import Turtle, Screen
 from random import randint
+from tkinter import messagebox
 
 
 screen = Screen()
 screen.setup(width=500, height=400)
 screen.colormode(255)
 is_race_on = False
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 
 def generate_colored_turtles():
     turtle_box = []
-    colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+    # colors = ["red", "orange", "yellow", "green", "blue", "purple"]
     for turtle_color in colors:
         t = Turtle(shape="turtle")
         t.color(turtle_color)
@@ -38,26 +40,53 @@ def send_turtles_start(turtle_rainbow):
 
 turtle_rainbow = generate_colored_turtles()
 send_turtles_start(turtle_rainbow)
-user_bet = screen.textinput(
-    title="Make your bet",
-    prompt="Which turtle you think will win the race?"
-    "\nEnter a color: "
-)
+# user_bet = screen.textinput(
+#     title="Make your bet",
+#     prompt="Which turtle you think will win the race?"
+#     "\nEnter a color: "
+# )
 
+
+included_color = False
+wrong_color_msg = ""
+while not included_color:
+    user_bet = screen.textinput(
+        title="Make your bet",
+        prompt=f"{wrong_color_msg}" +
+        "Which turtle you think will win the race?"
+        "\nEnter a color: "
+    )
+    if not user_bet:
+        break
+    for color in colors:
+        if user_bet == color:
+            included_color = True
+            break
+    wrong_color_msg = "Color not included!\n"
+
+
+winning_turtle = ""
+race_over = False
 if user_bet:
-    is_race_on = True
-
-while is_race_on:
-
-    for turtle in turtle_rainbow:
-        if turtle.xcor() > 230:
-            is_race_on = False
-            winning_turtle = turtle.pencolor()
-            if winning_turtle == user_bet:
-                print(f"You've won! The {winning_turtle} is the winner!")
-            else:
-                print(f"You've lost! The {winning_turtle} is the winner!")
-        turtle.forward(randint(1, 10))
+    while True:
+        for turtle in turtle_rainbow:
+            turtle.forward(randint(1, 10))
+            if turtle.xcor() > 230:
+                winning_turtle = turtle.pencolor()
+                race_over = True
+                break
+        if race_over:
+            break
+    if winning_turtle == user_bet:
+        # print()
+        messagebox.showinfo(
+            message=f"You've won! The {winning_turtle} is the winner!")
+    else:
+        messagebox.showinfo(
+            message=f"You've lost! The {winning_turtle} is the winner!")
+else:
+    messagebox.showinfo(message="You didn't bet. See the next one")
+    screen.bye()
 
 
 screen.mainloop()
